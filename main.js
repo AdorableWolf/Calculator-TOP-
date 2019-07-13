2b09c2245a388e56ae6e98f3738a2aee12bf98fb
AdorableWolf <sanket1998@outlook.com>
2019-07-13 13:31:03 +1200

    Initial commit along with completed HTML and CSS files

---
+++ main.js
@@ -0,0 +1,21 @@
+//functions to do the calculations.
+function add(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function percentage(a){
+    return a / 100;
+}
+
